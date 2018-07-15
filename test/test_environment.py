efc3272d68b559e6d8bb101bd61d42d97afff3cc
Josef Friedrich <josef@friedrich.rocks>
2018-07-15 09:01:51 +0200

    Test argument not_during_daytime

--- test/test_environment.py
+++ test/test_environment.py
@@ -188,6 +188,27 @@ class TestClassReachableLights(unittest.TestCase):
         self.assertEqual(lights.get_light_ids(), [1])
         self.assertTrue(lights._bridge[1].on)
 
+    @freeze_time('2000-01-01 23:00:00')
+    def test_parameter_not_during_daytime_true_by_night(self):
+        lights = self.get_reachable_lights([1, True], not_during_daytime=True)
+        self.assertEqual(lights.get_light_ids(), [1])
+
+    @freeze_time('2000-01-01 23:00:00')
+    def test_parameter_not_during_daytime_false_by_night(self):
+        lights = self.get_reachable_lights([1, True], not_during_daytime=False)
+        self.assertEqual(lights.get_light_ids(), [1])
+
+    @freeze_time('2000-01-01 12:00:00')
+    def test_parameter_not_during_daytime_true_by_day(self):
+        lights = self.get_reachable_lights([1, True], not_during_daytime=True)
+        self.assertEqual(lights.get_light_ids(), [])
+
+    @freeze_time('2000-01-01 12:00:00')
+    def test_parameter_not_during_daytime_false_by_day(self):
+        lights = self.get_reachable_lights([1, True], not_during_daytime=False)
+        self.assertEqual(lights.get_light_ids(), [1])
+        self.assertTrue(lights._bridge[1].on)
+
     @unittest.skipIf(not INTERNET_CONNECTIFITY, 'No internet connectifity')
     def test_parameter_not_host_up(self):
         lights = self.get_reachable_lights([1, True], not_host_up='8.8.8.8:53')
